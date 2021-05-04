637603b8a9f9fee04f8fec85c25202858019e866
Michael DiSanto <mdisanto928@gmail.com>
2021-05-04 10:30:37 -0400

    update code in employee.test.js

--- _test_/Employee.test.js
+++ _test_/Employee.test.js
@@ -1,35 +1,35 @@
 const Employee = require("../lib/Employee");
 
- test("Can initiate employee instance", () =>{
+ describe("Can initiate employee instance", () =>{
      const e = new Employee();
      expect(typeof(e)).toBe("object");
  });
 
- test("Can set via constructor argument"), () => {
+ describe("Can set name via constructor argument"), () => {
 
  };
 
- test("Can set id via constructor element"), () => {
+ describe("Can set id via constructor element"), () => {
 
 };
 
-test("Can set email via constructor argument"), () => {
+describe("Can set email via constructor argument"), () => {
 
 };
 
-test("Can get name via getname()"), () => {
+describe("Can get name via getname()"), () => {
 
 };
 
-test("Can get Id via getId()"), () => {
+describe("Can get Id via getId()"), () => {
 
 };
 
-test("Can get email via getEmail()"), () => {
+describe("Can get email via getEmail()"), () => {
 
 }; 
 
-test("getRole() should return employee "), () => {
+describe("getRole() should return employee "), () => {
 // Anthony has "" for employee?
 };
 
