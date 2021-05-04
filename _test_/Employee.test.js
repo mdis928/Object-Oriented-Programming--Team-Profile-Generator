c94ee1d6cba253a8c9a1169fad70a656b8143629
Michael DiSanto <mdisanto928@gmail.com>
2021-05-04 10:10:15 -0400

    added tests in employee.test.js

--- _test_/Employee.test.js
+++ _test_/Employee.test.js
@@ -4,3 +4,32 @@ const Employee = require("../lib/Employee");
      const e = new Employee();
      expect(typeof(e)).toBe("object");
  });
+
+ test("Can set via constructor argument"), () => {
+
+ };
+
+ test("Can set id via constructor element"), () => {
+
+};
+
+test("Can set email via constructor argument"), () => {
+
+};
+
+test("Can get name via getname()"), () => {
+
+};
+
+test("Can get Id via getId()"), () => {
+
+};
+
+test("Can get email via getEmail()"), () => {
+
+}; 
+
+test("getRole() should return "Employee""), () => {
+
+};
+ 
