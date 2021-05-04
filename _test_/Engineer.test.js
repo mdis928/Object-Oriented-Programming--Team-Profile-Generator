92795348492742c67455c5eaf807e9145615d019
Michael DiSanto <mdisanto928@gmail.com>
2021-05-04 10:37:42 -0400

    added code to Engineer.test.js

---
+++ _test_/Engineer.test.js
@@ -0,0 +1,14 @@
+const Engineer = require("../lib/Engineer");
+
+describe("Can set GitHub account via constructor", () =>{
+
+});
+
+describe("getRole() should return Engineer", () =>{
+
+});
+
+describe("Can get GitHub username via getGithub", () =>{
+
+});
+
