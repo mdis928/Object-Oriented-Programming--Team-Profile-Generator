36575e4c37300bfefbc6bc6aad1858c186b4a7aa
Michael DiSanto <mdisanto928@gmail.com>
2021-05-04 10:45:09 -0400

    Added code in intern.test.js

---
+++ _test_/Intern.test.js
@@ -0,0 +1,13 @@
+const Intern = require("../lib/Intern");
+
+describe("Can set school via constructor", () => {
+
+});
+
+describe("getRole() should return Intern", () => {
+
+});
+
+describe("Can get school via getSchool", () => {
+
+});
