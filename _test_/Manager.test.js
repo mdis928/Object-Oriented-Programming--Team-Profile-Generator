a20b2e3eee0530851423e56e26fa9c6de5ea75fb
Michael DiSanto <mdisanto928@gmail.com>
2021-05-04 10:48:47 -0400

    added code in manager.test.js

---
+++ _test_/Manager.test.js
@@ -0,0 +1,13 @@
+const Manager = require("../lib/Manager");
+
+describe("Can set office number via constructor argument", () => {
+
+});
+
+describe("getRole() should return Manager", () => {
+
+});
+
+describe("Can get office number via getOffice", () => {
+
+});
