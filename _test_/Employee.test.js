01e37e35e751cddf89cb8e3cd6c0ec8a9167bbcc
Michael DiSanto <mdisanto928@gmail.com>
2021-05-05 12:32:16 -0400

    added code to employee.test.js

--- _test_/Employee.test.js
+++ _test_/Employee.test.js
@@ -9,10 +9,42 @@ describe ("Employee Test", () => {
         expect(e.email).toEqual("mike@gmail.com"); 
     });
 
-    describe("Can set name via constructor argument"), () => {
-    
+    it("Can set name via constructor argument"), () => {
+        const name = "Mike";
+        const e = new Employee (name);
+        expect(e.name).toEqual ("Mike");
+    };
+
+    it("Can set id via constructor element"), () => {
+        const id = "123";
+        const e = new Employee (id);
+        expect(e.id).toEqual (id);
 
     };
+    
+    it("Can set email via constructor argument"), () => {
+        const email = 'mike@gmail.com';
+        const e = new Employee;
+        expect(e.email).toEqual (email);
+    };
+    
+    it("Can get name via getname()"), () => {
+    
+    };
+    
+    it("Can get Id via getId()"), () => {
+    
+    };
+    
+    it("Can get email via getEmail()"), () => {
+    
+    }; 
+    
+    it("getRole() should return employee "), () => {
+    // Anthony has "" for employee?
+    };
+
+
 
 
 })
@@ -21,27 +53,5 @@ describe ("Employee Test", () => {
 
  
 
- describe("Can set id via constructor element"), () => {
 
-};
-
-describe("Can set email via constructor argument"), () => {
-
-};
-
-describe("Can get name via getname()"), () => {
-
-};
-
-describe("Can get Id via getId()"), () => {
-
-};
-
-describe("Can get email via getEmail()"), () => {
-
-}; 
-
-describe("getRole() should return employee "), () => {
-// Anthony has "" for employee?
-};
 
