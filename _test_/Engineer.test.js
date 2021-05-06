21eae8e27ccf46e6e1fda03f35a23c8b12a746b1
Michael DiSanto <mdisanto928@gmail.com>
2021-05-06 08:37:07 -0400

    cleaned up code

--- _test_/Engineer.test.js
+++ _test_/Engineer.test.js
@@ -6,21 +6,18 @@ describe("Engineer Test", () => {
         const GitHubTest = "mdis928";
         const e = new Engineer ("Foo", "0", "mike@gmail.com", GitHubTest);
         expect(e.gitHub).toEqual(GitHubTest)
-
     });
     
     it("getRole() should return Engineer", () =>{
         const testRole = "Engineer";
         const e = new Engineer ();
         expect(e.getRole()).toEqual(testRole);
-    
     });
     
     it("Can get GitHub username via getgitHub()", () => {
         const getGitHubTest = "ztf37";
         const e = new Engineer ("foo", "0", "ztf37@gmail.com", getGitHubTest);
         expect(e.getgitHub()).toEqual(getGitHubTest)
-    
     });
     
 })
