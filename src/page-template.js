e548b823504683f3150c08178ff54875d915c058
Michael DiSanto <mdisanto928@gmail.com>
2021-05-06 14:00:08 -0400

    add and deleted some code

--- src/page-template.js
+++ src/page-template.js
@@ -1,6 +1,6 @@
 // Note: this is similar to a markdown file
 
-const Intern = require("../lib/Intern");
+
 
 const generateManager = Manager => {
     return `
@@ -13,7 +13,7 @@ const generateManager = Manager => {
         <ul class="list-group">
             <li class="list-group-item">ID: ${Manager.getId()}</li>
             <li class="list-group-item">Email: <a href="mailto:${Manager.getEmail()}">${Manager.getEmail()}</a></li>
-            <li class="list-group-item">Office number: ${manager.getofficeNumber()}</li>
+            <li class="list-group-item">Office number: ${Manager.getofficeNumber()}</li>
         </ul>
     </div>
 </div>
@@ -25,20 +25,20 @@ const generateEngineer = Engineer => {
     <div class="card employee-card">
     <div class="card-header">
         <h2 class="card-title">${Engineer.getName()}</h2>
-        <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.getRole()}</h3>
+        <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${Engineer.getRole()}</h3>
     </div>
     <div class="card-body">
         <ul class="list-group">
-            <li class="list-group-item">ID: ${manager.getId()}</li>
-            <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
-            <li class="list-group-item">Office number: ${manager.getgitHub()}</li>
+            <li class="list-group-item">ID: ${Engineer.getId()}</li>
+            <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}">${Engineer.getEmail()}</a></li>
+            <li class="list-group-item">Office number: ${Engineer.getgitHub()}</li>
         </ul>
     </div>
 </div>
     `;
 };
 
-const generateEngineer = Intern => {
+const generateIntern = Intern => {
     return `
     <div class="card employee-card">
     <div class="card-header">
@@ -55,4 +55,6 @@ const generateEngineer = Intern => {
 </div>
     `;
 };
+
+
 
