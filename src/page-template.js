18eeb265ea5723b40cab28dc65c28d5ccffa26bd
Michael DiSanto <mdisanto928@gmail.com>
2021-05-06 13:56:47 -0400

    added engineer and intern

--- src/page-template.js
+++ src/page-template.js
@@ -1,5 +1,7 @@
 // Note: this is similar to a markdown file
 
+const Intern = require("../lib/Intern");
+
 const generateManager = Manager => {
     return `
     <div class="card employee-card">
@@ -29,7 +31,25 @@ const generateEngineer = Engineer => {
         <ul class="list-group">
             <li class="list-group-item">ID: ${manager.getId()}</li>
             <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
-            <li class="list-group-item">Office number: ${manager.getOfficeNumber()}</li>
+            <li class="list-group-item">Office number: ${manager.getgitHub()}</li>
+        </ul>
+    </div>
+</div>
+    `;
+};
+
+const generateEngineer = Intern => {
+    return `
+    <div class="card employee-card">
+    <div class="card-header">
+        <h2 class="card-title">${Intern.getName()}</h2>
+        <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${Intern.getRole()}</h3>
+    </div>
+    <div class="card-body">
+        <ul class="list-group">
+            <li class="list-group-item">ID: ${Intern.getId()}</li>
+            <li class="list-group-item">Email: <a href="mailto:${Intern.getEmail()}">${Intern.getEmail()}</a></li>
+            <li class="list-group-item">Office number: ${Intern.getschool()}</li>
         </ul>
     </div>
 </div>
