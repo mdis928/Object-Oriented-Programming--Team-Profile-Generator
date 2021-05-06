01b090645c72c0955855c41aec95f0930ed01a66
Michael DiSanto <mdisanto928@gmail.com>
2021-05-06 08:10:15 -0400

    added and testing code in Engineer.test.js. Finally working

--- _test_/Engineer.test.js
+++ _test_/Engineer.test.js
@@ -1,19 +1,26 @@
+const Employee = require("../lib/Employee");
 const Engineer = require("../lib/Engineer");
 
 describe("Engineer Test", () => {
 
-    it("Can set GitHub account via constructor", () =>{
-        const gitHub = "GithubuserID";
-        const e = new Engineer (gitHub);
-        expect(e.github).toBe(gitHub);
+    it("Can set GitHub account via constructor", () => {
+        const GitHubTest = "mdis928";
+        const e = new Engineer ("Foo", "0", "mike@gmail.com", GitHubTest);
+        expect(e.gitHub).toEqual(GitHubTest)
 
     });
     
     it("getRole() should return Engineer", () =>{
+        const testRole = "Engineer";
+        const e = new Engineer ();
+        expect(e.getRole()).toEqual(testRole);
     
     });
     
-    it("Can get GitHub username via getGithub", () =>{
+    it("Can get GitHub username via getgitHub()", () => {
+        const getGitHubTest = "ztf37";
+        const e = new Engineer ("foo", "0", "ztf37@gmail.com", getGitHubTest);
+        expect(e.getgitHub()).toEqual(getGitHubTest)
     
     });
     
