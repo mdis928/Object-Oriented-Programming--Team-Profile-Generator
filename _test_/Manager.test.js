032a221ffaf03380658511192b4619753c9c4793
Michael DiSanto <mdisanto928@gmail.com>
2021-05-06 08:34:13 -0400

    added code and tested manager.test.js

--- _test_/Manager.test.js
+++ _test_/Manager.test.js
@@ -3,15 +3,21 @@ const Manager = require("../lib/Manager");
 describe ("Manager Test", () => {
 
     it("Can set office number via constructor argument", () => {
-
+        const testOfficeNumber = "789";
+        const e = new Manager ("foo", "0", "test@gmail.com", testOfficeNumber);
+        expect(e.officeNumber).toEqual (testOfficeNumber);
     });
     
     it("getRole() should return Manager", () => {
-    
+        const testRole = "Manager";
+        const e = new Manager ();
+        expect(e.getRole()).toEqual(testRole);
     });
     
-    it("Can get office number via getOffice", () => {
-    
+    it("Can get office number via getofficeNumber", () => {
+        const testValue = "1497";
+        const e = new Manager ("foo", "0", "test@gmail.com", testValue);
+        expect(e.getofficeNumber()).toEqual(testValue);
     });
 
 })
