050670a16fd73bf547c6866f163fda79eb5eec58
Michael DiSanto <mdisanto928@gmail.com>
2021-05-08 09:48:27 -0400

    added code to get head for index.html.

--- src/page-template.js
+++ src/page-template.js
@@ -1,7 +1,24 @@
 // Note: this is similar to a markdown file
 
 let html = []
-let string = "";
+let string = `
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>My Team</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/c502137733.js"></script>
+</head>
+
+<body>
+`
 
 const generateManager = Manager => {
     return `
@@ -80,7 +97,7 @@ module.exports = (team) => {
     );
     for (let i = 0; i < html.length; i += 1) {
         string += html[i]
-      } return string
+      } return string + '</body> </html>'
     
 }
 
